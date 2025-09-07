8c113765fd52512565991b2a06d89ed6ca62439c
BRIAN <omondiomimo@gmail.com>
2025-09-07 04:50:21 +0300

    updated

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -1,4 +1,6 @@
-User
+
+ CREATE TABLE User (
+
 user_id: Primary Key, UUID, Indexed
 first_name: VARCHAR, NOT NULL
 last_name: VARCHAR, NOT NULL
@@ -6,9 +8,9 @@ email: VARCHAR, UNIQUE, NOT NULL
 password_hash: VARCHAR, NOT NULL
 phone_number: VARCHAR, NULL
 role: ENUM (guest, host, admin), NOT NULL
-created_at: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP
+created_at: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP ) ;
   
-**Property**
+CREATE TABLLE Property (
   
 property_id: Primary Key, UUID, Indexed
 host_id: Foreign Key, references User(user_id)
@@ -17,9 +19,9 @@ description: TEXT, NOT NULL
 location: VARCHAR, NOT NULL
 pricepernight: DECIMAL, NOT NULL
 created_at: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP
-updated_at: TIMESTAMP, ON UPDATE CURRENT_TIMESTAMP
+updated_at: TIMESTAMP, ON UPDATE CURRENT_TIMESTAMP );
   
-**Booking**
+ CREATE TABLE Booking(
   
 booking_id: Primary Key, UUID, Indexed
 property_id: Foreign Key, references Property(property_id)
@@ -28,29 +30,29 @@ start_date: DATE, NOT NULL
 end_date: DATE, NOT NULL
 total_price: DECIMAL, NOT NULL
 status: ENUM (pending, confirmed, canceled), NOT NULL
-created_at: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP
+created_at: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP );
   
-**Payment**
+CREATE TABLE Payment (
   
 payment_id: Primary Key, UUID, Indexed
 booking_id: Foreign Key, references Booking(booking_id)
 amount: DECIMAL, NOT NULL
 payment_date: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP
-payment_method: ENUM (credit_card, paypal, stripe), NOT NULL
+payment_method: ENUM (credit_card, paypal, stripe), NOT NULL );
   
-**Review**
+CREATE TABLE Review (
   
 review_id: Primary Key, UUID, Indexed
 property_id: Foreign Key, references Property(property_id)
 user_id: Foreign Key, references User(user_id)
 rating: INTEGER, CHECK: rating >= 1 AND rating <= 5, NOT NULL
 comment: TEXT, NOT NULL
-created_at: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP
+created_at: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP );
   
-**Message**
+CREATE TABLE messages (
   
 message_id: Primary Key, UUID, Indexed
 sender_id: Foreign Key, references User(user_id)
 recipient_id: Foreign Key, references User(user_id)
 message_body: TEXT, NOT NULL
-sent_at: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP
+sent_at: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP );
